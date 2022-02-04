3fb74968a35031c0e85ab496b7a50272570d0b8d
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-02-04 09:58:25 +0900

    Update videolist.js

--- videolist.js
+++ videolist.js
@@ -29,4 +29,58 @@ export const videos = [
         playlist: "brock",
         url: "BOG61HmvEGw"
     },
+    {
+        id: 6,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 5/10] #BrockChallenge",
+        playlist: "brock",
+        url: "dvb0ISc2Ki0"
+    },
+    {
+        id: 7,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 6/10] #BrockChallenge",
+        playlist: "brock",
+        url: "o809RZJlKOM"
+    },
+    {
+        id: 8,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 7/10] #BrockChallenge",
+        playlist: "brock",
+        url: "geBKOFIBfvc"
+    },
+    {
+        id: 9,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 8/10] #BrockChallenge",
+        playlist: "brock",
+        url: "AQeLQzuRAG0"
+    },
+    {
+        id: 10,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 9/10] #BrockChallenge",
+        playlist: "brock",
+        url: "8yg3-BWnGaM"
+    },
+    {
+        id: 11,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 10/10] #BrockChallenge",
+        playlist: "brock",
+        url: "xhetWtYiy3M"
+    },
+    {
+        id: 12,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 10a/10] #BrockChallenge",
+        playlist: "brock",
+        url: "vNE56t1snEM"
+    },
+    {
+        id: 13,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 10b/10] #BrockChallenge",
+        playlist: "brock",
+        url: "H9JrzduIVAQ"
+    },
+    {
+        id: 14,
+        title: "How many Pokemon beat Brock on #MinimumBattles? [Ep. 10c/10] #BrockChallenge",
+        playlist: "brock",
+        url: "AstqRV9n9Zc"
+    },
 ]
