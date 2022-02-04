9634ef293abe721cc4e69aa1088bd66b4e8ae6d2
teoperalez <93870128+teoperalez@users.noreply.github.com>
2022-02-04 11:26:59 +0900

    updating Videos Page

--- pages/videos.js
+++ pages/videos.js
@@ -1,9 +1,58 @@
+import styles from '../styles/Videos.module.css'
+import { useState, useRef, useEffect } from 'react';
+
+
 export default function Videos({videos}) {
-    return (
-        <div>
-            {   videos.map((video, index) => (
-                <iframe key={video.url} width="320" height="180" src={"https://www.youtube.com/embed/"+`${video.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+  
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const card = useRef();
+
+  const brockVideos = videos.filter(vid => vid.playlist.includes('brock'));
+  const mistyVideos = videos.filter(vid => vid.playlist.includes('misty'));
+  const rivalVideos = videos.filter(vid => vid.playlist.includes('rival'));
+
+
+  useEffect(() => {
+    setWidth(card.current.scrollWidth)
+    setHeight(card.current.scrollHeight/2)
+  }, [])
+
+  return (
+        <div className={styles.container}>
+            <h1>Brock Minimum Battles Runs</h1>
+            <div className={styles.wrapper}>
+            {brockVideos.map((video, index) => (
+                <div ref={card} key={video.url} className={styles.card}>
+                  <div>
+                    <iframe width={width} height={height} id="video" src={"https://www.youtube.com/embed/"+`${video.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                  </div>
+                  <h2>{video.title}</h2>
+                </div>
             ))}
+            </div>
+            <h1>Misty Minimum Battles Runs</h1>
+            <div className={styles.wrapper}>
+            {mistyVideos.map((video, index) => (
+                <div ref={card} key={video.url} className={styles.card}>
+                  <div>
+                    <iframe width={width} height={height} id="video" src={"https://www.youtube.com/embed/"+`${video.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                  </div>
+                  <h2>{video.title}</h2>
+                </div>
+            ))}
+            </div>
+            <h1>Rival 5 (Silph Co.) Minimum Battles Runs</h1>
+            <div className={styles.wrapper}>
+            {rivalVideos.map((video, index) => (
+                <div ref={card} key={video.url} className={styles.card}>
+                  <div>
+                    <iframe width={width} height={height} id="video" src={"https://www.youtube.com/embed/"+`${video.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                  </div>
+                  <h2 className={styles.title}>{video.title}</h2>
+                </div>
+            ))}
+            </div>
             </div>
     )
 }
