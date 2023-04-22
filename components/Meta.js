a68643c9b38f026a22e8199c35ed0477316d915d
Teo Peralez <93870128+teoperalez@users.noreply.github.com>
2023-04-22 10:44:53 +0900

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -3,9 +3,7 @@ import Head from 'next/head'
 export default function Meta({title, description, tags}) {
     return (
         <Head>
-            <script src="https://unpkg.com/@microsoft/signalr@latest/dist/browser/signalr.min.js"></script>
-            <script src="https://unpkg.com/vue@latest/dist/vue.global.js"></script>
-            <script src="http://localhost:8085/dist/gameHookMapperClient.js"></script>
+            
             <title>RBY Pokemon Challenges || {title}</title>
             <meta charSet="UTF-8"></meta>
             <meta name="description" content={description}/>
